9a5297c33803721e2266c744debd86b1946eec2a
nazidasyed <syeda.nazida@gmail.com>
2022-10-08 22:43:24 -0400

    first commit

---
+++ src/javasession21/Java5.java
@@ -0,0 +1,10 @@
+package javasession21;
+
+public class Java5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/javasession23/Java2.java
@@ -0,0 +1,10 @@
+package javasession23;
+
+public class Java2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
